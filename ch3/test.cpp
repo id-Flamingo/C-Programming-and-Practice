2da04295fb34840769420e539bc423556899b463
Ian Dowdy <idgithub@outlook.com>
2024-06-11 19:04:32 -0400

    forgot to commit

--- ch3/test.cpp
+++ ch3/test.cpp
@@ -4,11 +4,23 @@
 using namespace std;
 
 int main(){ 
-    string previous = " ";
-    string current;
-    while (cin>>current){
-        if(previous == current)
-            cout << "repeated word: " << current << '\n';
-        previous = current;
+    // string previous = " ";
+    // string current;
+    // while (cin>>current){
+    //     if(previous == current)
+    //         cout << "repeated word: " << current << '\n';
+    //     previous = current; 
+    // }
+
+    double d = 0;
+    while (cin>>d){
+
+        int i = d;
+        char c = i;
+        int i2 = c;
+        cout << "d==" << d
+             << " i==" << i
+             << " i2==" << i2 
+             << " char(" <<c<<")\n";
     }
 }
